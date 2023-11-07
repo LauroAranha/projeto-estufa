d1aeb686c60f4e809f60b0c89f86153891814e7a
Gabriel Germano <galermano@MacBook-Pro-de-Gabriel.local>
2023-11-07 16:45:52 -0300

    dev: initialize react

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from '@carbon/react';
+
+import './app.scss';
+
+const App = () => {
+  return (
+    <>
+      <div>test</div>
+      <Button>Test</Button>
+    </>
+  );
+};
+
+export default App;
