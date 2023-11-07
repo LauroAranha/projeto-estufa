9e16fde0db8b9bf2d41b7b63069e6f5b882fc37d
Pirdo <100035058+Pirdo@users.noreply.github.com>
2023-11-06 21:41:12 -0300

    Bomba v1

--- esp-code/Bomba.ino
+++ esp-code/Bomba.ino
@@ -1,69 +1,21 @@
-#include <DHTesp.h>
-#include <ESP8266WiFi.h>
+#define minibomba 2
+#define sensor 0
 
-char ssid[] = "Fatec111";
-char password[] = "123456789";
-
-const char* http_site = "iot-phi.vercel.app";
-const int http_port = 443;
-const char* http_path = "/cadastrar"; 
-
-DHTesp dht;
-
-static bool b = false;
+int umidade;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  int wifiCounter = 0;
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED && wifiCounter < 20) {
-    delay(1000);
-    Serial.println("Conectando no WiFi...");
-    wifiCounter++;
-  }
-
-  if(wifiCounter >= 20) {
-    Serial.println("Falha ao conectar o Wifi...");
-    b = true;
-  } else {
-    Serial.println("WiFi conectado!");
-    dht.setup(3, DHTesp::DHT11);
-  }
-
+  Serial.begin(9600);
+  pinMode(minibomba, HIGH);
 }
 
-void loop() {
-  WiFiClientSecure client;
-  if (b) {
-    return;
+void loop() {  
+
+  umidade = ( 100 - ( (analogRead(sensor)/1023.00) * 100 ) );
+  Serial.println(umidade);
+  if(umidade <= 65) {
+    pinMode(minibomba, HIGH);
+  } else {
+    pinMode(minibomba, LOW);
   }
-  client.setInsecure();
-
-  delay(dht.getMinimumSamplingPeriod());
-
-  float umidade = dht.getHumidity();
-  float temperatura = dht.getTemperature();
-
-  Serial.print("Umidade: ");
-  Serial.print(umidade, 1);
-  Serial.print("%\tTemperatura: ");
-  Serial.print(temperatura, 1);
-  Serial.println("°C");
-
-  String url = "https://" + String(http_site) + http_path + "/" + String(temperatura) + "/" + String(umidade);
-
-  Serial.println("Fazendo request!");
-
-  if (!client.connect(http_site, http_port)) {
-    Serial.println("Falha na conexão com o servidor");
-    delay(5000);
-    return;
-  }
-
-   client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + http_site + "\r\n" + "Connection: close\r\n\r\n");
- 
-    client.stop();
-
-    delay(60000);
+  delay(500);
 }
