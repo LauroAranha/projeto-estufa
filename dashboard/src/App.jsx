4d04414b56b8f21e42c72a4fbb96704ccf64909b
Gabriel Germano <galermano@MBP-de-Gabriel.mshome.net>
2023-11-09 20:07:09 -0300

    dev: add navegation logic in App.jsx and header

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import Header from './Components/Header';
-
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Overview from './pages/Overview';
 import './app.scss';
 
+// Importando Compotens
+import Header from './Components/Header/Header';
+
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="container-principal">
+        <Routes>
+          <Route index path="/" Component={Overview} />
+        </Routes>
+      </div>
+    </>
+  );
 };
 
 export default App;
--- dashboard/src/Components/Header/Header.jsx
+++ dashboard/src/Components/Header/Header.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 // Importando estilos
 import './_header.scss';
 
 // Importando imagens
+import logo from '../../../public/logo.svg';
+import menu from '../../icons/menu.svg';
 
-// Importando Icons
+// Importando Components
+import Navbar from '../Navbar/Navbar';
 
 const Header = () => {
   return (
     <div className="header-container">
-      <div className="container-user">
-        <div
-          style={{ display: 'flex', flexDirection: 'column', marginLeft: 16 }}
-        >
-          <p className="user-name">ALO</p>
-        </div>
+      <div className="container-logo">
+        <img src={logo} alt="logo image" className="logo-img" />
       </div>
+      <Navbar />
+      <Button className="button-menu">
+        <img src={menu} alt="menu icon" className="menu-icon" />
+      </Button>
     </div>
   );
 };
