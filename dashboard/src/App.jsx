0453f5a5040fdaed92b401e84a5a0372fb528a06
joel <joelnunes974@gmail.com>
2023-12-01 11:09:55 -0300

    viewport

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Overview from './pages/Overview';
 import './app.scss';
+import { Helmet } from 'react-helmet';
 
 // Importando Compotens
 import Header from './Components/Header/Header';
@@ -9,6 +10,12 @@ import Header from './Components/Header/Header';
 const App = () => {
   return (
     <>
+      <Helmet>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no"
+        />
+      </Helmet>
       <Header />
       <div className="container-principal">
         <Routes>
